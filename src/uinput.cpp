880f9cd81c227c465cbd4e16a7516853429b8d8f
southoz <adriangrant@iinet.net.au>
2021-05-16 16:31:27 +1000

    Update uinput.cpp

--- src/uinput.cpp
+++ src/uinput.cpp
@@ -25,6 +25,7 @@
 #include <unistd.h>
 #include <stdexcept>
 #include <string.h>
+#include <cmath.h>
 #include <sys/epoll.h>
 
 /*
@@ -550,11 +551,11 @@ namespace {
 			case EV_ABS: {
 				switch(ev.code) {
 				// left stick
-				case ABS_X: { ev.code = ABS_X; ev.value = (ev.value*(32768*2)/4095-32768) * -1;} break;
-				case ABS_Y: { ev.code = ABS_Y; ev.value = (ev.value*(32768*2)/4095-32768) * -1;} break;
+				case ABS_X: { ev.code = ABS_X; ev.value = (Round(ev.value*18.204)) * -1;} break;
+				case ABS_Y: { ev.code = ABS_Y; ev.value = (Round(ev.value*18.204)) * -1;} break;
 				// right stick
-				case ABS_RX: { ev.code = ABS_RX; ev.value = ev.value*(32768*2)/4095-32768;} break;
-				case ABS_RY: { ev.code = ABS_RY; ev.value = ev.value*(32768*2)/4095-32768;} break;
+				case ABS_RX: { ev.code = ABS_RX; ev.value = (Round(ev.value*18.204));} break;
+				case ABS_RY: { ev.code = ABS_RY; ev.value = (Round(ev.value*18.204));} break;
 				// do not report any other axis
 				default:
 					return false;
